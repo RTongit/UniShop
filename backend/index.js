0fbf6b9b50b45ca64d07c16f080fdc46052bb535
meme wizard <rohantamuli27@gmail.com>
2026-08-27 20:05:10 +0530

    commits

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,12 @@ const port = process.env.PORT;
 const app = express();
 const httpServer = createServer(app)
 
-app.use(cors({origin:"http://localhost:3000",credentials : true}))
+app.use(cors(
+    {
+        origin:["http://localhost:3000",process.env.FRONTEND_URL],
+        credentials : true
+    }
+))
 app.use(express.json({limit : "10MB"}))
 app.use(cookieParser())
 app.use('/api/auth',authRouter);
