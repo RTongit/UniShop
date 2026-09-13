55ddcc5e96c11e8dec392dc40ed761cb83c17e91
meme wizard <rohantamuli27@gmail.com>
2026-09-13 13:40:54 +0530

    feat: enhance search functionality with sorting options, improve ItemCard layout for better responsiveness, and update Navbar input styling

--- frontend/src/app/(routes)/search/page.jsx
+++ frontend/src/app/(routes)/search/page.jsx
@@ -4,6 +4,7 @@ import NoItem from "@/app/components/NoItem";
 import Spinner from "@/app/components/Spinner";
 import { useAuthStore } from "@/app/store/authStore";
 import { useItemStore } from "@/app/store/itemStore";
+import { ChevronDown } from "lucide-react";
 import { useRouter, useSearchParams } from "next/navigation";
 import { useEffect, useState } from "react";
 
@@ -15,65 +16,64 @@ export default function Search() {
   const title = searchParams.get("title");
   const [sort, setSort] = useState("");
   const [dummyItems, setDummyItems] = useState();
-  console.log(sort)
 
   useEffect(() => {
     if (!authUser) router.replace("/login");
     if (title) {
-        async function fetchItems() {
-            const result = await searchItems(title);
-            if(result==null) return
-            setDummyItems(result);
-        }
-        fetchItems();
+      async function fetchItems() {
+        const result = await searchItems(title);
+        if (result == null) return;
+        setDummyItems(result);
+      }
+      fetchItems();
     }
   }, [router, authUser, title, searchItems]);
 
   function handleChange(e) {
     setSort(e.target.value);
 
-    // If newest to oldest filter applied ,then : 
-    if(e.target.value=="newest") {
-        setDummyItems((prev)=>{
-            const temp = [...prev]
-            temp.sort((a,b)=>b.createdAt.localeCompare(a.createdAt) )
-            return temp
-        })
+    // If newest to oldest filter applied ,then :
+    if (e.target.value == "newest") {
+      setDummyItems((prev) => {
+        const temp = [...prev];
+        temp.sort((a, b) => b.createdAt.localeCompare(a.createdAt));
+        return temp;
+      });
     }
 
-    // If oldest to newest filter applied ,then : 
-    else if(e.target.value=="oldest") {
-        setDummyItems((prev)=>{
-            const temp = [...prev]
-            temp.sort((a,b)=>a.createdAt.localeCompare(b.createdAt))
-            return temp
-        })
+    // If oldest to newest filter applied ,then :
+    else if (e.target.value == "oldest") {
+      setDummyItems((prev) => {
+        const temp = [...prev];
+        temp.sort((a, b) => a.createdAt.localeCompare(b.createdAt));
+        return temp;
+      });
     }
 
-    // For price low to high : 
-    else if(e.target.value=="cheap") {
-        setDummyItems((prev)=>{
-            const temp = [...prev]
-            temp.sort((a,b)=>{
-                if(a.price<b.price) return -1;
-                else if(a.price>b.price) return 1;
-                return 0;
-            })
-            return temp
-        })
+    // For price low to high :
+    else if (e.target.value == "cheap") {
+      setDummyItems((prev) => {
+        const temp = [...prev];
+        temp.sort((a, b) => {
+          if (a.price < b.price) return -1;
+          else if (a.price > b.price) return 1;
+          return 0;
+        });
+        return temp;
+      });
     }
 
-    // For price high to low : 
-    else if(e.target.value=="expensive") {
-        setDummyItems((prev)=>{
-            const temp = [...prev]
-            temp.sort((a,b)=>{
-                if(a.price>b.price) return -1;
-                else if(a.price<b.price) return 1;
-                return 0;
-            })
-            return temp
-        })
+    // For price high to low :
+    else if (e.target.value == "expensive") {
+      setDummyItems((prev) => {
+        const temp = [...prev];
+        temp.sort((a, b) => {
+          if (a.price > b.price) return -1;
+          else if (a.price < b.price) return 1;
+          return 0;
+        });
+        return temp;
+      });
     }
   }
 
@@ -84,46 +84,38 @@ export default function Search() {
 
   return (
     <div className="px-6 py-6">
-
       {/* Container of filter and item list */}
-      <div className="flex gap-x-2">
+      <div className="flex md:flex-row flex-col md:gap-x-2 gap-y-2">
 
         {/* Filter section */}
-        <section className="flex flex-col gap-y-5">
-          <div>
-            <h2 className="text-xl font-semibold text-slate-900">
-              Browse Items
-            </h2>
-            <p className="mt-1 text-sm text-slate-500">
-              Find items that match your preference.
-            </p>
-          </div>
+        <section className="flex md:items-start gap-x-3 relative ">
+          <select
+            id="sort"
+            value={sort}
+            onChange={(e) => {
+              handleChange(e);
+            }}
+            className="appearance-none border-2 border-gray-400 rounded-md min-w-[176px] w-full
+               bg-white outline-none px-3 py-2 text-md font-medium text-slate-700  transition"
+          >
+            <option value="" disabled>
+              Sort By
+            </option>
+            <option value="newest">Newest</option>
+            <option value="oldest">Oldest</option>
+            <option value="cheap">Price: Low to High</option>
+            <option value="expensive">Price: High to Low</option>
+          </select>
 
-          <div className="flex items-center gap-3">
-            <label
-              htmlFor="sort"
-              className="text-sm font-medium text-slate-600"
-            >
-              Sort by
-            </label>
-
-            <select
-              id="sort"
-              value={sort}
-              onChange={(e)=>{handleChange(e)}}
-              className="rounded-xl border border-slate-200 bg-white px-4 py-2.5 text-sm font-medium text-slate-700 outline-none transition focus:border-blue-500 focus:ring-2 focus:ring-blue-100"
-            >
-              <option value="" disabled>Select options</option> 
-              <option value="newest">Newest</option>
-              <option value="oldest">Oldest</option>
-              <option value="cheap">Price: Low to High</option>
-              <option value="expensive">Price: High to Low</option>
-            </select>
-          </div>
+          {/* Custom arrow down */}
+          <ChevronDown
+            size={17}
+            className="absolute right-2 top-3 pointer-events-none"
+          />
         </section>
 
         {/*list of items*/}
-        <div className="flex flex-col gap-y-2">
+        <div className="flex flex-col gap-y-5">
           {dummyItems?.map((item) => (
             <ItemCard key={item._id} item={item} />
           ))}
--- frontend/src/app/components/ItemCard.js
+++ frontend/src/app/components/ItemCard.js
@@ -2,22 +2,21 @@ import Link from "next/link";
 
 export default function ItemCard({ item }) {
   return (
-    <Link href={`/item/${item._id}`} className="h-[300px]">
-      <div className="flex h-full">
+    <Link href={`/item/${item._id}`} className="flex md:h-[300px] h-[200px]" >
 
         {/* Picture */}
-        <div className="min-w-[280px]">
+        <div className="md:min-w-[280px] min-w-[140px] h-full bg-stone-100 p-2">
           <img src={item.photos[0]} className="w-full h-full object-contain"></img>
         </div>
 
         <div className="p-3 overflow-hidden flex flex-col gap-y-3">
           {/* Title */}
-          <p className="text-sm font-semibold text-gray-900 leading-snug line-clamp-2 mb-1">
+          <p className="text-sm font-bold text-gray-900 leading-snug line-clamp-2 mb-1">
             {item.title}
           </p>
 
           {/* Content */}
-          <p className="hover:underline h-[100px] overflow-hidden">{item.description}</p>
+          <p className="hover:underline max-h-[100px] overflow-hidden">{item.description}</p>
 
           {/* Price */}
           <p className="font-bold text-gray-900">
@@ -25,7 +24,6 @@ export default function ItemCard({ item }) {
           </p>
         </div>
 
-      </div>
     </Link>
   );
 }
